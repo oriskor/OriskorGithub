14b1bdf5c06562205b05e5562702d4f2d4d79f22
ysunil340 <38753897+ysunil340@users.noreply.github.com>
2018-05-01 12:20:29 +0530

    currupt
    currupt

--- EDI/EDI/Models/Provider/HeaderDetailsInformation.cs
+++ EDI/EDI/Models/Provider/HeaderDetailsInformation.cs
@@ -17,7 +17,7 @@ namespace EDI.Provider
         public DataSet GetHeaderDetailInformation()
         {
             DataSet ds = new DataSet();
-            ds=SqlHelper.ExecuteProcudere("SPO_GetHeaderDetailInformation", null);
+            ds=SqlHelper.ExecuteProcudere("GetHeaderDetailInformation", null);
             return ds;
 
         }
